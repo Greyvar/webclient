eba10adeaed6953a40d94b2e827ccc066ed9ad85
jamesread <contact@jread.com>
2023-04-18 05:39:50 +0100

    Various updates from several months ago

--- src/js/scenes/GridScene.js
+++ src/js/scenes/GridScene.js
@@ -8,50 +8,38 @@ export default class GridScene extends Phaser.Scene {
 
   constructor(grid) {
     super(new Date().toString())
-    this.grid = grid;
+    this.grid = grid
   }
 
 
   preload() {
-    this.load.setBaseURL('http://mindstorm/greyvar-webclient/res/')
+    this.load.setBaseURL('https://mindstorm/greyvar-webclient/res/')
     this.load.image('construct.png', 'img/textures/tiles/construct.png')
 
-    this.load.spritesheet('ss_playerBob.png', 'img/textures/entities/ss_playerBob.png', {
-      frameWidth: 15,
-      frameHeight: 15,
-    })
-
-    this.load.spritesheet('chest.png', 'img/textures/entities/chest.png', {
-      frameWidth: 15,
-      frameHeight: 15,
-    })
-
-    this.load.spritesheet('ss_flowers.png', 'img/textures/entities/ss_flowers.png', {
-      frameWidth: 15,
-      frameHeight: 15,
-    })
-
-
+    for (const entdef of window.gameState.entdefs.entries()) {
+      this.preloadEntdefSprite(entdef[1])
+    }
 
     this.load.atlas('water', 'img/textures/fluids/water.png', 'atlas/water.json')
 
     this.layerGrid = this.add.layer()
     this.layerEntities = this.add.layer()
 
-    this.preloadAllGridTextures(phaser)
+    this.preloadAllGridTextures()
     this.preloaded = true
   }
 
+  preloadEntdefSprite(entdef) {
+    this.load.spritesheet(entdef.name, 'img/textures/entities/' + entdef.texture, {
+      frameWidth: 15,
+      frameHeight: 15,
+    })
+  }
+
   create() {
-    //this.cameras.main.setSize(320, 256)
-
-//    this.cameras.main.setBounds(0, 0, 100, 100);
-//    this.cameras.main.centerOn(200, 200);
-//    this.cameras.main.setZoom(2)
-
     this.anims.create({
       key: 'water',
-      frames: this.anims.generateFrameNames('water', { prefix: '', end: 2}), 
+      frames: this.anims.generateFrameNames('water', { prefix: '', end: 2}),
       repeat: -1,
       duration: 1500,
     })
@@ -64,19 +52,20 @@ export default class GridScene extends Phaser.Scene {
     this.renderEntities()
   }
 
+  mnuClicked() {
+    console.log("mnu!")
+  }
+
   update() {
     this.updateCursorKeys()
+    this.updateGamepad()
+//    this.updateInputPointer()
 //    this.renderGrid()
 //    this.renderEntities()
 
   }
 
-  updateCursorKeys() {
-    if (this.cursors.left.isDown)   window.serverConnection.sendMoveRequest({x: -1, y: 0});
-    if (this.cursors.right.isDown)  window.serverConnection.sendMoveRequest({x: 1, y: 0});
-    if (this.cursors.up.isDown)     window.serverConnection.sendMoveRequest({x: 0, y: -1});
-    if (this.cursors.down.isDown)   window.serverConnection.sendMoveRequest({x: 0, y: 1});
-
+  updateInputPointer(){
     let p = this.input.activePointer
 
     if (p.isDown) {
@@ -88,6 +77,30 @@ export default class GridScene extends Phaser.Scene {
     }
   }
 
+  updateCursorKeys() {
+    if (this.cursors.left.isDown)   window.serverConnection.sendMoveRequest({x: -1, y: 0});
+    if (this.cursors.right.isDown)  window.serverConnection.sendMoveRequest({x: 1, y: 0});
+    if (this.cursors.up.isDown)     window.serverConnection.sendMoveRequest({x: 0, y: -1});
+    if (this.cursors.down.isDown)   window.serverConnection.sendMoveRequest({x: 0, y: 1});
+  }
+
+  updateGamepad() {
+    if (this.input.gamepad.total === 0) {
+      return
+    }
+
+    var pad = this.input.gamepad.getPad(0)
+
+    if (pad.axes.length) {
+      var x = pad.axes[6].getValue()
+      var y = pad.axes[7].getValue()
+
+      console.log(x, y)
+
+      window.serverConnection.sendMoveRequest({x: x, y: y})
+    }
+  }
+
   renderEntities() {
     for (const ent of Object.keys(this.entities)) {
       this.renderEntity(this.entities[ent])
@@ -96,9 +109,9 @@ export default class GridScene extends Phaser.Scene {
 
   renderEntity(ent) {
     if (ent.img == null) {
-      let img;
+      let img
 
-      if (ent.texture == "ss_playerBob.png") {
+      if (ent.definition == 'player') {
         img = this.physics.add.sprite(ent.x, ent.y, ent.texture)
         img.setCollideWorldBounds(true)
 
@@ -107,10 +120,10 @@ export default class GridScene extends Phaser.Scene {
         img = this.add.sprite(ent.x, ent.y, ent.texture)
       }
 
-      this.createEntityAnimations(img, ent.texture)
+      this.createEntityAnimations(img, ent.definition)
 
       img.setOrigin(0)
-//      img.play('idle')
+      img.play(ent.initialState)
 
       ent.img = img
 
@@ -121,42 +134,48 @@ export default class GridScene extends Phaser.Scene {
     //this.add.image(ent.x, ent.y, ent.texture)
   }
 
-  createEntityAnimations(img, tex) {
-    img.anims.create({
-      key: 'idle',
-      frameRate: 2,
-      frames: this.anims.generateFrameNumbers(tex, {start: 0, end: 3}),
-      repeat: -1
-    });
+  createEntityAnimations(img, definition) {
+    const entdef = window.gameState.entdefs.get(definition)
 
-    if (tex == "ss_playerBob.png") {
+    for (const state of entdef.states) {
+//      console.log('creating animation for ', definition, 'state:', state.name, 'with frames:', state.frames)
+
+      img.anims.create({
+        key: state.name,
+        frameRate: 2,
+        frames: this.anims.generateFrameNumbers(definition, {frames: state.frames}),
+        repeat: -1
+      })
+    }
+
+    if (definition == 'player') {
       img.anims.create({
         key: 'walkLeft',
         frameRate: 8,
-        frames: this.anims.generateFrameNumbers(tex, {start: 4, end: 7}),
+        frames: this.anims.generateFrameNumbers(definition, {start: 4, end: 7}),
         repeat: 0,
-      });
+      })
 
       img.anims.create({
         key: 'walkRight',
         frameRate: 8,
-        frames: this.anims.generateFrameNumbers(tex, {start: 8, end: 11}),
+        frames: this.anims.generateFrameNumbers(definition, {start: 8, end: 11}),
         repeat: 0,
-      });
+      })
 
       img.anims.create({
         key: 'walkUp',
         frameRate: 8,
-        frames: this.anims.generateFrameNumbers(tex, {start: 12, end: 15}),
+        frames: this.anims.generateFrameNumbers(definition, {start: 12, end: 15}),
         repeat: 0,
-      });
+      })
 
       img.anims.create({
         key: 'walkDown',
         frameRate: 8,
-        frames: this.anims.generateFrameNumbers(tex, {start: 16, end: 19}),
+        frames: this.anims.generateFrameNumbers(definition, {start: 16, end: 19}),
         repeat: 0,
-      });
+      })
     }
   }
 
@@ -164,17 +183,17 @@ export default class GridScene extends Phaser.Scene {
     const tiles = this.grid.allTiles()
 
     for (const index in tiles) {
-      this.renderTile(phaser, tiles[index])
+      this.renderTile(tiles[index])
     }
   }
 
-  renderTile(phaser, tile) {
+  renderTile(tile) {
     const tileSize = 16
 
     const posX = (tileSize / 2) + (tile.col * tileSize)
     const posY = (tileSize / 2) + (tile.row * tileSize)
 
-    if (tile.textureName == "water.png") {
+    if (tile.textureName == 'water.png') {
       const water = this.add.sprite((tile.col*tileSize), (tile.row*tileSize), 'water').setOrigin(0).play('water')
 
       this.layerGrid.add(water)
@@ -182,12 +201,13 @@ export default class GridScene extends Phaser.Scene {
     }
 
 
+    /*
     if (true) {
-//      this.add.text((tile.col * tileSize), (tile.row*tileSize), tile.textureName, {fontFamily: "sans-serif", fontSize: 8, color: "black"})
+      //      this.add.text((tile.col * tileSize), (tile.row*tileSize), tile.textureName, {fontFamily: "sans-serif", fontSize: 8, color: "black"})
 
       //this.add.text(0, 0, 'Hello World', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
-
     }
+    */
 
     const tex = this.add.image(posX, posY, tile.textureName)
     this.layerGrid.add(tex)
@@ -196,20 +216,20 @@ export default class GridScene extends Phaser.Scene {
     tex.flipY = tile.textureVerticalFlip
   }
 
-  preloadAllGridTextures(phaser) {
+  preloadAllGridTextures() {
     for (const tile of this.grid.allTiles()) {
-      let r = this.load.image(tile.textureName, 'img/textures/tiles/' + tile.textureName)
+      this.load.image(tile.textureName, 'img/textures/tiles/' + tile.textureName)
     }
   }
 
   renderConnectionResponse(r) {
-    this.text = "Server: " + r.serverVersion
+    this.text = 'Server: ' + r.serverVersion
   }
 
   onEntitySpawn(ent) {
-    console.log("ent spawn", ent.texture, ent.entityId)
+    console.log('ent spawn', ent.definition, ent.initialState, ent.entityId, ent.x, ent.y)
     ent.img = null
-  
+
     this.entities[ent.entityId] = ent
   }
 
@@ -217,7 +237,7 @@ export default class GridScene extends Phaser.Scene {
     let ent = this.entities[entpos.entityId]
 
     if (ent == null) {
-      console.log("cannot find ent to reposition", entpos.entityId)
+      console.log('cannot find ent to reposition', entpos.entityId)
       return
     }
 
@@ -226,16 +246,16 @@ export default class GridScene extends Phaser.Scene {
 
       if (ent.img.x != entpos.x || ent.img.y != entpos.y) {
         if (entpos.x > ent.img.x) {
-          ent.img.play("walkLeft")
+          ent.img.play('walkLeft')
         } else if (entpos.x < ent.img.x) {
-          ent.img.play("walkRight")
+          ent.img.play('walkRight')
         } else if (entpos.y > ent.img.y) {
-          ent.img.play("walkDown")
+          ent.img.play('walkDown')
         } else {
-          ent.img.play("walkUp")
+          ent.img.play('walkUp')
         }
 
-        ent.img.playAfterRepeat("idle")
+        ent.img.playAfterRepeat('idle')
 
         ent.img.x = entpos.x
         ent.img.y = entpos.y
@@ -245,5 +265,21 @@ export default class GridScene extends Phaser.Scene {
       }
 
     }
+  }
+
+  onEntityChange(ec) {
+    console.log('entchange', ec)
+
+    let ent = this.entities[ec.entityId]
+
+    if (ent == null) {
+      console.log('cannot find ent to change', ec.entityId)
+      return
+    }
+
+    let newState = window.gameState.entdefs.get(ent.definition)
+    //    console.log("newstate", ec.newState, newState)
+
+    ent.img.play(ec.newState)
   }
 }
